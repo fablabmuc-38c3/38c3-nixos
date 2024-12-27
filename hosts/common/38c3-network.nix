cb67f9a146436f252b2eac28abc2a9efae987431
Lukas Holzner <meistergig@gmail.com>
2024-12-27 15:31:50 +0100

    undo

--- hosts/common/38c3-network.nix
+++ hosts/common/38c3-network.nix
@@ -9,22 +9,6 @@
         prefixLength = 23;
       }];
   };
-  networking.networkmanager.enable = true;
-  networking.networkmanager.ensureProfiles.profiles= {
-    fiber = {
-      connection = {
-        id = "fiber";
-        uuid = "5bc1572f-c4b3-36c4-8444-9a4c347158b2";
-        type = "ethernet";
-        autoconnect-priority = "-999";
-        interface-name = "enp43s0";
-        timestamp = "1735308281";
-      };
-      ipv6 = {
-        addr-gen-mode = "0";
-      };
-    };
-  };
 
   services.lldpd.enable = true;
 
