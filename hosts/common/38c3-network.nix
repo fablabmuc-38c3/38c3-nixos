f1b1ff3388a877f306475651ebd4ea3966f55a83
DragonHunter274 <schurgel@gmail.com>
2025-12-28 13:41:40 +0100

    fix github proxy

--- hosts/common/38c3-network.nix
+++ hosts/common/38c3-network.nix
@@ -19,12 +19,12 @@
   services.tailscale.useRoutingFeatures = "both";
 
   networking.extraHosts = ''
-    2a01:4f8:c010:d56::2 github.com
-    2a01:4f8:c010:d56::3 api.github.com
-    2a01:4f8:c010:d56::4 codeload.github.com
-    2a01:4f8:c010:d56::6 ghcr.io
-    2a01:4f8:c010:d56::7 pkg.github.com npm.pkg.github.com maven.pkg.github.com nuget.pkg.github.com rubygems.pkg.github.com
-    2a01:4f8:c010:d56::8 uploads.github.com
+    2a01:4f8:1c1f:b8da::2 github.com
+    2a01:4f8:1c1f:b8da::3 api.github.com
+    2a01:4f8:1c1f:b8da::4 codeload.github.com
+    2a01:4f8:1c1f:b8da::6 ghcr.io
+    2a01:4f8:1c1f:b8da::7 pkg.github.com npm.pkg.github.com maven.pkg.github.com nuget.pkg.github.com rubygems.pkg.github.com
+    2a01:4f8:1c1f:b8da::8 uploads.github.com
     2606:50c0:8000::133 objects.githubusercontent.com www.objects.githubusercontent.com release-assets.githubusercontent.com gist.githubusercontent.com repository-images.githubusercontent.com camo.githubusercontent.com private-user-images.githubusercontent.com avatars0.githubusercontent.com avatars1.githubusercontent.com avatars2.githubusercontent.com avatars3.githubusercontent.com cloud.githubusercontent.com desktop.githubusercontent.com support.github.com
     2606:50c0:8000::154 support-assets.githubassets.com github.githubassets.com opengraph.githubassets.com github-registry-files.githubusercontent.com github-cloud.githubusercontent.com
   '';
